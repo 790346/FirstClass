cbd8dc7201a3d03f2d9570677437e5cb20b448c3
790346 <790346@seq.org>
2016-08-23 21:34:17 -0700

    Add files via upload

---
+++ FirstClassRunner.java
@@ -0,0 +1,9 @@
+public class FirstClassRunner {
+    
+    public static void main(String[] args) {
+        
+        FirstClass var = new FirstClass();
+        FirstClass var2 = new FirstClass();
+        
+    }
+}
